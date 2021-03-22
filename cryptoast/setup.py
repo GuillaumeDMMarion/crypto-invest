4e1a60db4d57c5e10ead3eb1f2220d2888933f91
GuillaumeDMMarion <guillaume.d.m.marion@gmail.com>
2021-03-22 13:55:15 +0100

    Add setu^^

---
+++ cryptoast/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='cryptoast',
+    version='0.1',
+    author='Guillaume Marion',
+    packages=['cryptoast'],
+    url='http://github.com/GuillaumeDMMarion/crypto-invest',
+    description='Library for managing crypto currency assets',
+    install_requires=[
+        "numpy", "pandas", "plotly", "tqdm", "requests",
+    ],
+)
