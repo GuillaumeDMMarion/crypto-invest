54d98edd7041e1feaf115695957a7c065de9a8e6
GuillaumeDMMarion <guillaume.d.m.marion@gmail.com>
2021-06-01 23:36:18 +0200

    Add signals to observation

--- cryptoast/model/agents.py
+++ cryptoast/model/agents.py
@@ -221,10 +221,11 @@ class SingleAssetEnv(gym.Env):
         self.allow_gaps = allow_gaps
         self.episode_steps = episode_steps
         self.action_space = gym.spaces.Discrete(3)
-        self.obs_cols = klmngr[klmngr.assets[0]].indicators.columns.to_series()
-        self.rel_indicators = self.obs_cols.str.startswith(self._rel_indicators_stem)
-        self.abs_indicators = self.obs_cols.str.startswith(self._abs_indicators_stem)
-        self.n_obs_cols = self.rel_indicators.sum() + self.abs_indicators.sum()
+        self.indicator_cols = klmngr[klmngr.assets[0]].indicators.columns.to_series()
+        self.signal_cols = klmngr[klmngr.assets[0]].signals.columns.to_series()
+        self.rel_indicators_mask = self.indicator_cols.str.startswith(self._rel_indicators_stem)
+        self.abs_indicators_mask = self.indicator_cols.str.startswith(self._abs_indicators_stem)
+        self.n_obs_cols = self.rel_indicators_mask.sum() + self.abs_indicators_mask.sum() + len(self.signal_cols)
         self.observation_space = gym.spaces.Box(low=-np.inf,
                                                 high=np.inf,
                                                 shape=(window*self.n_obs_cols+2*self.backtest.memory,),
@@ -253,9 +254,15 @@ class SingleAssetEnv(gym.Env):
         """
         # timestamp = timestamp + pd.Timedelta(days=1)
         close_values = self.kline.loc[:timestamp, :].close.values[-window:].reshape(-1, 1)
-        abs_indicator_values = self.kline.indicators.loc[:timestamp, self.abs_indicators].fillna(-1).values[-window:]
-        rel_indicator_values = self.kline.indicators.loc[:timestamp, self.rel_indicators].fillna(-1).values[-window:]
-        observation = np.hstack((abs_indicator_values, rel_indicator_values / close_values[-1]))
+        abs_signals = self.kline.signals.loc[:timestamp, :]
+        abs_signal_values = abs_signals.fillna(-1).values[-window:]
+        abs_indicators = self.kline.indicators.loc[:timestamp, self.abs_indicators_mask]
+        abs_indicator_values = abs_indicators.fillna(-1).values[-window:]
+        rel_indicators = self.kline.indicators.loc[:timestamp, self.rel_indicators_mask]
+        rel_indicator_values = rel_indicators.fillna(-1).values[-window:]
+        observation = np.hstack((abs_signal_values,
+                                 abs_indicator_values,
+                                 rel_indicator_values / close_values[-1]))
         periodic_values = np.array(self.backtest.history)[:, :2]
         transactions_history = np.hstack([0, 0] +
                                          [(periodic_values[:-1, _] != periodic_values[1:, _]).astype(int)
@@ -306,7 +313,8 @@ class SingleAssetEnv(gym.Env):
             fitted_scaler = scaler.fit(hist_close_values)
             setattr(self, 'scaler', fitted_scaler)
         # self.scaler.transform(observation_raw).T.ravel()
-        observation = np.hstack([self.scaler.fit_transform(observation_raw[:, [_]]) for _ in range(self.obs_cols)])
+        observation = np.hstack([self.scaler.fit_transform(observation_raw[:, [_]]) for _ in
+                                 range(self.indicator_cols)])
         # nr_of_transactions = (-1+2*sum(periodic_values[:-1, 1] != periodic_values[1:, 1])/(window-1)).reshape(-1, 1)
         transactions_history = np.hstack([0, 0] +
                                          [(periodic_values[:-1, _] != periodic_values[1:, _]).astype(int)
@@ -321,15 +329,16 @@ class SingleAssetEnv(gym.Env):
         previous_timestamp = timestamp - timedelta(hours=1)
         while True:
             try:
-                value_t0 = getattr(self.backtest, '_periodic')[previous_timestamp]['value']
+                # value_t0 = getattr(self.backtest, '_periodic')[previous_timestamp]['value']
                 # price_t0 = getattr(self.kline, 'close')[previous_timestamp]
                 break
             except KeyError:
                 previous_timestamp -= timedelta(hours=1)
         value_t1 = getattr(self.backtest, '_periodic')[timestamp]['value']
-        # price_t1 = getattr(self.kline, 'close')[timestamp]
-        reward = (value_t1-value_t0)/value_t0
-        reward = reward-1 if reward <= 0 else 1+reward
+        price_t1 = getattr(self.kline, 'close')[timestamp]
+        reward = value_t1 - price_t1
+        # reward = (value_t1-value_t0)/value_t0
+        # reward = reward-1 if reward <= 0 else 1+reward
         # value_d = ((value_t1-value_t0)/value_t0)
         # price_d = ((price_t1-price_t0)/price_t0)
         # return value_d / price_d
