3dadcf8ef134be16320b7c03fa296df8b015660e
GuillaumeDMMarion <guillaume.d.m.marion@gmail.com>
2021-12-21 13:41:21 +0100

    Add minor changes

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
 from setuptools import setup
 
 setup(
-    name='cryptoast',
-    version='0.1',
-    author='Guillaume Marion',
-    packages=['cryptoast'],
-    url='http://github.com/GuillaumeDMMarion/crypto-invest',
-    description='Library for managing crypto currency assets',
+    name="cryptoast",
+    version="0.1",
+    author="Guillaume Marion",
+    packages=["cryptoast"],
+    url="http://github.com/GuillaumeDMMarion/crypto-invest",
+    description="Library for managing crypto currency assets",
     install_requires=[
-        "numpy", "pandas", "plotly", "tqdm", "requests", "python-binance", "pip"
+        "numpy",
+        "pandas",
+        "plotly",
+        "tqdm",
+        "requests",
+        "python-binance",
+        "pip",
     ],
 )
